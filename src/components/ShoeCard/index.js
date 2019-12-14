b277b8cac575776c3d736417f542918246a3aadd
nicoletownley <natownley@gmail.com>
2019-12-14 07:50:54 -0600

    update ShoeCard for functionality

--- src/components/ShoeCard/index.js
+++ src/components/ShoeCard/index.js
@@ -1,3 +1,15 @@
 import React from 'react';
 import './style.css';
 
+//set it up so function will run when pictures are clicked on
+function ShoeCard(props) {
+    return (
+<div className='card'>
+    <div className = 'img-container' onClick={()=>{props.gymShoe(props.id)}} scores={props.scores}/>
+    <img alt={props.id}src = {props.image}/>
+</div>
+)};
+
+
+
+    export default ShoeCard;
