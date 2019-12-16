a59ab52cd411509bb06cae10afd2f089b95c5b16
nicoletownley <natownley@gmail.com>
2019-12-16 12:43:45 -0600

    Updates

---
+++ precache-manifest.d5b07974824f4392378fc4c0bb8c4c70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0bd3834841ceb2df0f2e96fc8d9a4d6",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "c68ebd254b3a791aefd8",
+    "url": "/clickygame/static/css/main.17228c99.chunk.css"
+  },
+  {
+    "revision": "95876dc9ca6c4e14ab20",
+    "url": "/clickygame/static/js/2.28c93151.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clickygame/static/js/2.28c93151.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c68ebd254b3a791aefd8",
+    "url": "/clickygame/static/js/main.6055748d.chunk.js"
+  },
+  {
+    "revision": "417fd3d673572284b9ca",
+    "url": "/clickygame/static/js/runtime-main.8d5882b4.js"
+  }
+]);
